03c3dec1884511e10bac5c0e602968c2af7ee7a0
Matt Himrod <matt@matthimrod.com>
2023-03-10 21:54:56 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# busylight_client
+
+Raspberry Pi busy indicator light client
+
+## Requirements
+
+### Microsoft Teams/PowerShell Client
+
+* Microsoft PowerShell 7.x
+
+Start the client in a Windows Terminal/PowerShell window by using the following:
+
+```PowerShell
+wt.exe -w 0 new-tab pwsh busylight_teams_client.ps1
+```
---
+++ busylight_teams_client.ps1
@@ -0,0 +1,33 @@
+$Config = Get-Content -Raw ~\.dotfiles\busylight.json | ConvertFrom-Json
+$Host.UI.RawUI.WindowTitle = $Config.WindowTitle
+
+function Get-TimeStamp {
+    return "[{0:MM/dd/yy} {0:HH:mm:ss}]" -f (Get-Date)
+}
+
+$LastActivity = ""
+
+if ((Get-NetConnectionProfile).Name -match $Config.MyNetworkName) {
+    while($true) {
+        $LogContent = Get-Content $Config.TeamsLogFile -tail 1000 | Select-String -Pattern 'StatusIndicatorStateService\: Added (\w+) [^|]*'
+        $activity = $LogContent.Matches[$LogContent.Matches.Length - 1].Groups[1].Value
+
+        if ($null -ne $activity -and $activity -ne $LastActivity) {
+            $retries = $Config.MaxRetry
+            do {
+                Write-Output "$(Get-TimeStamp) Setting status to $activity"
+                try {
+                    $result = Invoke-RestMethod -Uri $Config.URL -Method 'Post' -Body @{ state = $activity }
+                    $LastActivity = $activity
+                    $retries = 0
+                } catch [System.Object] {
+                    $retries--
+                    Start-Sleep -Seconds $Config.RetryWait
+                }
+            } until ($retries -eq 0) 
+        }
+        Start-Sleep –Seconds $Config.PollingInterval 
+    }
+} else {
+    Write-Output "Not connected to home network!"
+}
