82af626857dadf7c203537cc0394e8dc7d103a31
Matt Himrod <matt@matthimrod.com>
2023-06-16 21:21:41 -0400

    Added try-finally to turn off the light on termination

--- busylight_teams_client.ps1
+++ busylight_teams_client.ps1
@@ -8,31 +8,37 @@ function Get-TimeStamp {
 $LastActivity = ""
 
 if ((Get-NetConnectionProfile).Name -match $Config.MyNetworkName) {
-    while($true) {
-        $LogContent = Get-Content $Config.TeamsLogFile -tail 2000 | Select-String -Pattern 'StatusIndicatorStateService\: Added (\w+) [^|]*'
-        if ($LogContent -and $LogContent.Matches.Length -ge 1) {
-            $activity = $LogContent.Matches[$LogContent.Matches.Length - 1].Groups[1].Value
-
-            if ($null -ne $activity -and $activity -ne $LastActivity) {
-                $retries = $Config.MaxRetry
-                do {
-                    Write-Output "$(Get-TimeStamp) Setting status to $activity"
-                    $prevProgressPreference = $Global:ProgressPreference
-                    try {
-                        $Global:ProgressPreference = 'SilentlyContinue'
-                        $null = Invoke-RestMethod -Uri $Config.URL -Method 'Post' -Body @{ state = $activity }
-                        $LastActivity = $activity
-                        $retries = 0
-                    } catch [System.Object] {
-                        $retries--
-                        Start-Sleep -Seconds $Config.RetryWait
-                    } finally {
-                        $Global:ProgressPreference = $prevProgressPreference
-                    }
-                } until ($retries -eq 0) 
+    try {
+        while($true) {
+            $LogContent = Get-Content $Config.TeamsLogFile -tail 2000 | Select-String -Pattern 'StatusIndicatorStateService\: Added (\w+) [^|]*'
+            if ($LogContent -and $LogContent.Matches.Length -ge 1) {
+                $activity = $LogContent.Matches[$LogContent.Matches.Length - 1].Groups[1].Value
+    
+                if ($null -ne $activity -and $activity -ne $LastActivity) {
+                    $retries = $Config.MaxRetry
+                    do {
+                        Write-Output "$(Get-TimeStamp) Setting status to $activity"
+                        $prevProgressPreference = $Global:ProgressPreference
+                        try {
+                            $Global:ProgressPreference = 'SilentlyContinue'
+                            $null = Invoke-RestMethod -Uri $Config.URL -Method 'Post' -Body @{ state = $activity }
+                            $LastActivity = $activity
+                            $retries = 0
+                        } catch [System.Object] {
+                            $retries--
+                            Start-Sleep -Seconds $Config.RetryWait
+                        } finally {
+                            $Global:ProgressPreference = $prevProgressPreference
+                        }
+                    } until ($retries -eq 0) 
+                }
             }
+            Start-Sleep –Seconds $Config.PollingInterval 
         }
-        Start-Sleep –Seconds $Config.PollingInterval 
+    }
+    finally {
+        # If something happens that terminates the script, try to turn off the light before we goooooo.....
+        $null = Invoke-RestMethod -Uri $Config.URL -Method 'Post' -Body @{ state = 'off' }
     }
 } else {
     Write-Output "Not connected to home network!"
